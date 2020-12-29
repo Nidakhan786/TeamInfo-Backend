e1e85bc8d4ef0f2e642733a2f4d3e5a9e9d73081
nkhan <nida.khan@soprasteria.com>
2020-12-30 00:25:58 +0530

    [Initial Commit]: Project Added with login and signup API

---
+++ controllers/user_controller.js
@@ -0,0 +1,57 @@
+/**
+ * User controller : All business logic goes here
+ */
+const User = require("../models/user_model");
+const bcrypt = require("bcryptjs");
+/**
+ * this method is to create the user
+ */
+exports.create = (req, res) => {
+  /**
+   * validation request
+   */
+  if (!req.body.email || !req.body.password || !req.body.first_name) {
+    return res.status(400).send({
+      message: "Required field can not be empty",
+    });
+  }
+  /**
+   * Create a user
+   */
+  const user = new User({
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 10),
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    emp_id: req.body.emp_id
+  });
+  /**
+   * Save user to database
+   */
+  user
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the User.",
+      });
+    });
+};
+/**
+ * Find all Users
+ */
+exports.findAll = (req, res) => {
+  User.find()
+    .sort({ name: -1 })
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error Occured",
+      });
+    });
+};
+
---
+++ models/user_model.js
@@ -0,0 +1,46 @@
+const mongoose = require("../db");
+const schema = new mongoose.Schema(
+  {
+    email: {
+      desc: "The user's email address.",
+      trim: true,
+      type: String,
+      index: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      desc: "user password",
+      trim: true,
+      type: String,
+      required: true,
+      select: false,
+    },
+    first_name: {
+      desc: "The user's first name.",
+      trim: true,
+      type: String,
+      required: true,
+    },
+    last_name: {
+      desc: "The user's last name.",
+      trim: true,
+      type: String,
+      required: true,
+    },
+    emp_id: {
+      desc: "The user's employee id.",
+      trim: true,
+      type: Number,
+      required: true,
+    },
+ 
+  },
+  {
+    strict: true,
+    versionKey: false,
+    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+  }
+);
+
+module.exports = mongoose.model("Users", schema);
